7002764b671df57f9bc88d8728670d72680b0563
ranim-99 <ranim.1999.ta@gmail.com>
2025-10-29 15:28:43 +0300

    arabic

--- src/components/RegisterForm/footer/Deadline.jsx
+++ src/components/RegisterForm/footer/Deadline.jsx
@@ -13,9 +13,9 @@ export default function Deadline() {
   return (
     <div className="flex bg-black text-white w-screen overflow-hidden items-center justify-center relative md:h-80 h-60">
       <Image src={NVDubaiDesktop} alt="Museum of the futur in Dubai"
-      className="size-full max-w-6xl ltr:ml-40 rtl:mr-40 object-fill md:block hidden" />
+      className="size-full max-w-6xl ltr:ml-40 rtl:mr-40 rtl:-scale-y-100 rtl:rotate-180 object-fill md:block hidden" />
       <Image src={NVDubai} alt="Museum of the futur in Dubai"
-      className="size-full max-w-6xl ltr:ml-10 rtl:mr-10 object-fill md:hidden block" />
+      className="size-full max-w-6xl ltr:ml-10 rtl:mr-10 rtl:-scale-y-100 rtl:rotate-180 object-fill md:hidden block" />
 
       <div className="absolute inset-0 z-10 space-y-2 flex items-center justify-center">
         <div className="flex flex-col items-start justify-between w-full gap-4 max-w-[954px] px-2 font-bold">
@@ -26,7 +26,7 @@ export default function Deadline() {
             {t('location')}
           </span>
           <a href="https://luma.com/8mrbuqpn" target="_blank" rel="noopener noreferrer" 
-          className="button-large flex items-center justify-start gap-2 group p-space">
+          className="button-large flex items-center justify-start gap-2 group p-space px-0">
             <span>{t('register')}</span>
             <MdOpenInNew className="text-mainGreen group-hover:text-mainGreenHover transition max-md:size-6" />
           </a>
