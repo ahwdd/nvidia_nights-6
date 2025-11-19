cf1babf26538717f41c08c99a9bdbcfcbc6c5a7d
ranim-99 <ranim.1999.ta@gmail.com>
2025-11-19 11:23:00 +0200

    gap

--- src/components/AboutEvent/AboutEvent.jsx
+++ src/components/AboutEvent/AboutEvent.jsx
@@ -5,7 +5,7 @@ function AboutEvent() {
   const t = useTranslations("AboutEvent");
   return (
     <div id="about-event" className="py-8 xl:py-10 flex items-center justify-center w-screen overflow-hidden">
-      <div className="flex flex-col max-md:gap-3 items-center w-full max-w-[66rem] mx-auto md:px-4 px-8 text-center text-large">
+      <div className="flex flex-col gap-3 items-center w-full max-w-[66rem] mx-auto md:px-4 px-8 text-center text-large">
           <p className="">{t("title1")}</p>
           <p className="">{t("title2")} </p>
           <p className="">{t("title3")} </p>
