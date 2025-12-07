c1d024b5a937f4d10b2f31c836e26a66a9490a61
ranim-99 <ranim.1999.ta@gmail.com>
2025-12-07 22:03:59 +0200

    not-found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,17 +1,24 @@
+"use client"
+import React, { Suspense } from "react";
 import Image from "next/image.js";
 import Link from "next/link.js";
 import { MoveLeft } from "lucide-react";
 import LanguageSwitcher from "@/components/Nav/LanguageSwitcher";
+
 function NotFound() {
   return (
     <>
       <div className="flex items-center justify-center flex-col gap-10 h-screen">
         <div className="flex flex-col gap-10 items-center">
-          <LanguageSwitcher />
+          <Suspense fallback={null}>
+            <LanguageSwitcher />
+          </Suspense>
+
           <h1 className="flex flex-col gap-10 text-3xl text-center font-bold justify-center items-center">
             This page is not found !
-            <Image src={`/not-found.png`} width={500} height={500} />
+            <Image src={`/not-found.png`} width={500} height={500} alt="not found" />
           </h1>
+
           <Link
             className="bg-mainGreen rounded w-fit p-2 flex items-center gap-3 justify-center"
             href={"/"}
