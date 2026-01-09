c24c6bf98b8513d38418463463fc6f10597f4b31
ranim-99 <ranim.1999.ta@gmail.com>
2026-01-09 16:25:07 +0200

    md:

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ function NewHero() {
     <div id="hero" className="w-screen overflow-hidden text-white px-4 md:px-32 md:h-[580px] h-[75vh] bg-black relative">
       <div className="new-hero absolute inset-0 size-full max-md:top-auto max-md:h-2/3 object-cover rtl:-scale-x-100 z-10" />
 
-      <div className="relative z-10 size-full flex flex-col md:justify-center items-center gap-5 text-center">
+      <div className="relative z-10 size-full flex flex-col justify-center items-center gap-5 text-center">
         {/* <Image src={`/nights-6/general/NV-Studio.png`} alt="Nvidia Studio" width={100} height={80}
         className="w-24 object-contain -mt-10 mb-10 max-md:pt-24" /> */}
         <div role="img" aria-label="Nvidia Studio" className="nv-studio-bg w-24 h-[80px] -mt-10 mb-10 max-md:pt-24"/>
